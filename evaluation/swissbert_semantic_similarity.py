1b76227f3f56c18d32d957f2f2d2e313f06e588f
jgrosj <152688373+jgrosj@users.noreply.github.com>
2024-02-26 00:25:40 +0100

    Update swissbert_semantic_similarity.py

--- evaluation/swissbert_semantic_similarity.py
+++ evaluation/swissbert_semantic_similarity.py
@@ -10,38 +10,55 @@ from sklearn.metrics.pairwise import cosine_similarity
 model_name="ZurichNLP/swissbert" # or "jgrosjean-mathesis/sentence-swissbert"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model.set_default_language("specify language")
+model.set_default_language("de_CH")
+model.eval()
 
 def generate_sentence_embedding(sentence):
 
     # tokenize input sentence
     inputs = tokenizer(sentence, padding=True, truncation=True, return_tensors="pt", max_length=512)
 
-    # set the model to evaluation mode
-    model.eval()
-
     # take tokenized input and pass it through the model
     with torch.no_grad():
         outputs = model(**inputs)
 
-    # extract average sentence embeddings from the last hidden layer
-    embedding = outputs.last_hidden_state.mean(dim=1)
+    # extract sentence embeddings via mean pooling
+    token_embeddings = outputs.last_hidden_state
+    attention_mask = inputs['attention_mask'].unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * attention_mask, 1)
+    sum_mask = torch.clamp(attention_mask.sum(1), min=1e-9)
+    embedding = sum_embeddings / sum_mask
 
     return embedding
 
-# set source file path
-file_paths = ["specify source file path"]
+# set source file paths
+summary_file_paths_DE = ["define path(s)"]
+content_file_paths_DE = ["define path(s)"]
+content_file_paths_FR = ["define path(s)"]
+content_file_paths_IT = ["define path(s)"]
+content_file_paths_RM = ["define path(s)"]
 
 # define summary and content lists
-ids = []
-summaries = {}
-contents = {}
-
-index = 0
-limit = 1000
+DE_ids = set()
+FR_ids = set()
+IT_ids = set()
+RM_ids = set()
+summaries_DE_DE = {}
+summaries_DE_FR = {}
+summaries_DE_IT = {}
+summaries_DE_RM = {}
+contents_DE = {}
+contents_FR = {}
+contents_IT = {}
+contents_RM = {}
+index_DE = 0
+index_FR = 0
+index_IT = 0
+index_RM = 0
+limit = 499
 
 print("extracting data...")
-for file_path in file_paths:
+for file_path in content_file_paths_DE:
     pattern = os.path.join(file_path, '*.json')
     json_files = glob.glob(pattern)
 
@@ -58,42 +75,187 @@ for file_path in file_paths:
             # get summary and content as strings and add them to lists
             else:
                 id = data.get("id", {})
+                DE_ids.add(id)
                 raw_content = data.get("content", {})
                 content_values = list(raw_content.values())
                 content = "\n".join([str(value) for value in content_values])
-                contents[id] = content
+                contents_DE[id] = content
                 
-                summary = "\n".join([str(value) for value in raw_summary.values()])
-                summaries[id] = summary
-
-            index += 1
-            if index >= limit:
+            index_DE += 1
+            if index_DE >= limit:
                 break
 
+for file_path in content_file_paths_FR:
+    pattern = os.path.join(file_path, '*.json')
+    json_files = glob.glob(pattern)
+
+    # iterate over articles
+    for json_file in json_files:
+        with open(json_file, 'r', encoding='utf-8') as article:
+            data = json.load(article)
+
+            # check if summary is empty
+            raw_summary = data.get("summary", {})
+            if not raw_summary:
+                continue
+
+            # get summary and content as strings and add them to lists
+            else:
+                id = data.get("id", {})
+                FR_ids.add(id)
+                raw_content = data.get("content", {})
+                content_values = list(raw_content.values())
+                content = "\n".join([str(value) for value in content_values])
+                contents_FR[id] = content
+                
+            index_FR += 1
+            if index_FR >= limit:
+                break
+
+for file_path in content_file_paths_IT:
+    pattern = os.path.join(file_path, '*.json')
+    json_files = glob.glob(pattern)
+
+    # iterate over articles
+    for json_file in json_files:
+        with open(json_file, 'r', encoding='utf-8') as article:
+            data = json.load(article)
+
+            # check if summary is empty
+            raw_summary = data.get("summary", {})
+            if not raw_summary:
+                continue
+
+            # get summary and content as strings and add them to lists
+            else:
+                id = data.get("id", {})
+                IT_ids.add(id)
+                raw_content = data.get("content", {})
+                content_values = list(raw_content.values())
+                content = "\n".join([str(value) for value in content_values])
+                contents_IT[id] = content
+                
+            index_IT += 1
+            if index_IT >= limit:
+                break
+
+for file_path in content_file_paths_RM:
+    pattern = os.path.join(file_path, '*.json')
+    json_files = glob.glob(pattern)
+
+    # iterate over articles
+    for json_file in json_files:
+        with open(json_file, 'r', encoding='utf-8') as article:
+            data = json.load(article)
+
+            # check if summary is empty
+            raw_summary = data.get("summary", {})
+            if not raw_summary:
+                continue
+
+            # get summary and content as strings and add them to lists
+            else:
+                id = data.get("id", {})
+                RM_ids.add(id)
+                raw_content = data.get("content", {})
+                content_values = list(raw_content.values())
+                content = "\n".join([str(value) for value in content_values])
+                contents_RM[id] = content
+                
+            index_RM += 1
+            if index_RM >= limit:
+                break
+
+for file_path in summary_file_paths_DE:
+    pattern = os.path.join(file_path, '*.json')
+    json_files = glob.glob(pattern)
+
+    # iterate over articles
+    for json_file in json_files:
+        with open(json_file, 'r', encoding='utf-8') as article:
+            data = json.load(article)
+
+            # check if summary is empty
+            raw_summary = data.get("summary", {})
+            if not raw_summary:
+                continue
+
+            # get summary and content as strings and add them to lists
+            else:
+                id = data.get("id", {})
+                if id in DE_ids:
+                    summary = "\n".join([str(value) for value in raw_summary.values()])
+                    summaries_DE_DE[id] = summary
+                if id in FR_ids:
+                    summary = "\n".join([str(value) for value in raw_summary.values()])
+                    summaries_DE_FR[id] = summary
+                if id in IT_ids:
+                    summary = "\n".join([str(value) for value in raw_summary.values()])
+                    summaries_DE_IT[id] = summary
+                if id in RM_ids:
+                    summary = "\n".join([str(value) for value in raw_summary.values()])
+                    summaries_DE_RM[id] = summary
+
 # compute embedding and set up embedding dictionaries
-summary_embeddings = {}
-content_embeddings = {}
+summary_embeddings_DE_DE = {}
+summary_embeddings_DE_FR = {}
+summary_embeddings_DE_IT = {}
+summary_embeddings_DE_RM = {}
+content_embeddings_DE = {}
+content_embeddings_FR = {}
+content_embeddings_IT = {}
+content_embeddings_RM = {}
 
 print("computing summary embeddings...")
-for id, summary in summaries.items():
+model.set_default_language("rm_CH")
+for id, summary in summaries_DE_DE.items():
     summary_embedding = generate_sentence_embedding(summary)
-    summary_embeddings[id] = summary_embedding
+    summary_embeddings_DE_DE[id] = summary_embedding
+
+for id, summary in summaries_DE_FR.items():
+    summary_embedding = generate_sentence_embedding(summary)
+    summary_embeddings_DE_FR[id] = summary_embedding
+
+for id, summary in summaries_DE_IT.items():
+    summary_embedding = generate_sentence_embedding(summary)
+    summary_embeddings_DE_IT[id] = summary_embedding
+
+for id, summary in summaries_DE_RM.items():
+    summary_embedding = generate_sentence_embedding(summary)
+    summary_embeddings_DE_RM[id] = summary_embedding
 
 print("computing content embeddings...")
-for id, content in contents.items():
+model.set_default_language("de_CH")
+for id, content in contents_DE.items():
     content_embedding = generate_sentence_embedding(content)
-    content_embeddings[id] = content_embedding
+    content_embeddings_DE[id] = content_embedding
+
+model.set_default_language("fr_CH")
+for id, content in contents_FR.items():
+    content_embedding = generate_sentence_embedding(content)
+    content_embeddings_FR[id] = content_embedding
+
+model.set_default_language("it_CH")
+for id, content in contents_IT.items():
+    content_embedding = generate_sentence_embedding(content)
+    content_embeddings_IT[id] = content_embedding
+
+model.set_default_language("rm_CH")
+for id, content in contents_RM.items():
+    content_embedding = generate_sentence_embedding(content)
+    content_embeddings_RM[id] = content_embedding
 
 # look for matches
+# look for matches
 print("calculating cosine similarities and getting matches...")
-predicted_matches = {}
+predicted_matches_DE = {}
 
-for summary_id, summary_embedding in summary_embeddings.items():
+for summary_id, summary_embedding in summary_embeddings_DE_DE.items():
     max_similarity = -1
     predicted_content_id = None
     cosine_score_dict = {}
 
-    for content_id, content_embedding in content_embeddings.items():
+    for content_id, content_embedding in content_embeddings_DE.items():
         cosine_score = cosine_similarity(summary_embedding, content_embedding)
         cosine_score_dict[content_id] = torch.from_numpy(cosine_score)
 
@@ -101,27 +263,153 @@ for summary_id, summary_embedding in summary_embeddings.items():
     predicted_content_id, max_cosine_score = max(cosine_score_dict.items(), key=lambda x: x[1])
 
     # assemble predictions in dictionary
-    predicted_matches[summary_id] = predicted_content_id
+    predicted_matches_DE[summary_id] = predicted_content_id
 
 # calculate evaluation scores
 print("setting up evaluation...")
-correct_matches = 0
-wrong_matches = 0
+correct_matches_DE = 0
+wrong_matches_DE = 0
 
 
-for summary_id, predicted_content_id in predicted_matches.items():
+for summary_id, predicted_content_id in predicted_matches_DE.items():
 
     # check if predictions are correct
     if summary_id == predicted_content_id:
-        correct_matches += 1
+        correct_matches_DE += 1
     else:
-        wrong_matches += 1
+        wrong_matches_DE += 1
 
-total_cases = correct_matches + wrong_matches
-accuracy = f"{100*(correct_matches / total_cases):.2f}"
+total_cases_DE = correct_matches_DE + wrong_matches_DE
+accuracy_DE = f"{100*(correct_matches_DE / total_cases_DE):.2f}"
 
 # print accuracy score
-print("\ntotal:\t\t", total_cases)
-print("correct:\t", correct_matches)
-print("wrong:\t\t", wrong_matches)
-print("\naccuracy:\t", accuracy, "%\n")
+print("\ntotal:\t\t", total_cases_DE)
+print("correct:\t", correct_matches_DE)
+print("wrong:\t\t", wrong_matches_DE)
+print("\naccuracy DE->DE:\t", accuracy_DE, "%\n")
+
+print("calculating cosine similarities and getting matches...")
+predicted_matches_FR = {}
+
+for summary_id, summary_embedding in summary_embeddings_DE_FR.items():
+    max_similarity = -1
+    predicted_content_id = None
+    cosine_score_dict = {}
+
+    for content_id, content_embedding in content_embeddings_FR.items():
+        cosine_score = cosine_similarity(summary_embedding, content_embedding)
+        cosine_score_dict[content_id] = torch.from_numpy(cosine_score)
+
+    # find the highest cosine similarity
+    predicted_content_id, max_cosine_score = max(cosine_score_dict.items(), key=lambda x: x[1])
+
+    # assemble predictions in dictionary
+    predicted_matches_FR[summary_id] = predicted_content_id
+
+# calculate evaluation scores
+print("setting up evaluation...")
+correct_matches_FR = 0
+wrong_matches_FR = 0
+
+
+for summary_id, predicted_content_id in predicted_matches_FR.items():
+
+    # check if predictions are correct
+    if summary_id == predicted_content_id:
+        correct_matches_FR += 1
+    else:
+        wrong_matches_FR += 1
+
+total_cases_FR = correct_matches_FR + wrong_matches_FR
+accuracy_FR = f"{100*(correct_matches_FR / total_cases_FR):.2f}"
+
+# print accuracy score
+print("\ntotal:\t\t", total_cases_FR)
+print("correct:\t", correct_matches_FR)
+print("wrong:\t\t", wrong_matches_FR)
+print("\naccuracy DE->FR:\t", accuracy_FR, "%\n")
+
+# look for matches
+print("calculating cosine similarities and getting matches...")
+predicted_matches_IT = {}
+
+for summary_id, summary_embedding in summary_embeddings_DE_IT.items():
+    max_similarity = -1
+    predicted_content_id = None
+    cosine_score_dict = {}
+
+    for content_id, content_embedding in content_embeddings_IT.items():
+        cosine_score = cosine_similarity(summary_embedding, content_embedding)
+        cosine_score_dict[content_id] = torch.from_numpy(cosine_score)
+
+    # find the highest cosine similarity
+    predicted_content_id, max_cosine_score = max(cosine_score_dict.items(), key=lambda x: x[1])
+
+    # assemble predictions in dictionary
+    predicted_matches_IT[summary_id] = predicted_content_id
+
+# calculate evaluation scores
+print("setting up evaluation...")
+correct_matches_IT = 0
+wrong_matches_IT = 0
+
+
+for summary_id, predicted_content_id in predicted_matches_IT.items():
+
+    # check if predictions are correct
+    if summary_id == predicted_content_id:
+        correct_matches_IT += 1
+    else:
+        wrong_matches_IT += 1
+
+total_cases_IT = correct_matches_IT + wrong_matches_IT
+accuracy_IT = f"{100*(correct_matches_IT / total_cases_IT):.2f}"
+
+# print accuracy score
+print("\ntotal:\t\t", total_cases_IT)
+print("correct:\t", correct_matches_IT)
+print("wrong:\t\t", wrong_matches_IT)
+print("\naccuracy DE->IT:\t", accuracy_IT, "%\n")
+
+# look for matches
+print("calculating cosine similarities and getting matches...")
+predicted_matches_RM = {}
+
+for summary_id, summary_embedding in summary_embeddings_DE_RM.items():
+    max_similarity = -1
+    predicted_content_id = None
+    cosine_score_dict = {}
+
+    for content_id, content_embedding in content_embeddings_RM.items():
+        cosine_score = cosine_similarity(summary_embedding, content_embedding)
+        cosine_score_dict[content_id] = torch.from_numpy(cosine_score)
+
+    # find the highest cosine similarity
+    predicted_content_id, max_cosine_score = max(cosine_score_dict.items(), key=lambda x: x[1])
+
+    # assemble predictions in dictionary
+    predicted_matches_RM[summary_id] = predicted_content_id
+
+# calculate evaluation scores
+print("setting up evaluation...")
+correct_matches_RM = 0
+wrong_matches_RM = 0
+
+
+for summary_id, predicted_content_id in predicted_matches_RM.items():
+
+    # check if predictions are correct
+    if summary_id == predicted_content_id:
+        correct_matches_RM += 1
+    else:
+        wrong_matches_RM += 1
+
+total_cases_RM = correct_matches_RM + wrong_matches_RM
+accuracy_RM = f"{100*(correct_matches_RM / total_cases_RM):.2f}"
+
+# print accuracy score
+print("\ntotal:\t\t", total_cases_RM)
+print("correct:\t", correct_matches_RM)
+print("wrong:\t\t", wrong_matches_RM)
+print("\naccuracy DE->RM:\t", accuracy_RM, "%\n")
+
